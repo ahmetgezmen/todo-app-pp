da984fee33e824043cf95b3636562cbf66926f5f
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-07-30 19:02:14 +0300

    firebase initialized

--- lib/core/initializing_app.dart
+++ lib/core/initializing_app.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'base_app.dart';
 
 initializingApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
-
