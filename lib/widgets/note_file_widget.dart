ca76d5a3409ee62558230a899cb0dc97e7d06e18
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-07-30 20:06:26 +0300

    note file widget created

---
+++ lib/widgets/note_file_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class NoteFileWidget extends StatefulWidget {
+  final String title;
+  const NoteFileWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<NoteFileWidget> createState() => _NoteFileWidgetState();
+}
+
+class _NoteFileWidgetState extends State<NoteFileWidget> {
+
+  @override
+  void didUpdateWidget(covariant NoteFileWidget oldWidget) {
+    if(oldWidget.title!=widget.title){
+      setState(() {});
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SvgPicture.asset("assets/svg/files-icon.svg"),
+        Text(widget.title, style: Theme.of(context).textTheme.titleSmall),
+      ],
+    );
+  }
+}
+
