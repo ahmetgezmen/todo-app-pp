bba27545d6baf8676281f62d7bdcd55b476949bf
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-07-30 18:58:29 +0300

    firebase core added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+
+    );
   }
 }
