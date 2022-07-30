8660471d6572885f76f281313885f1a5db110daa
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-07-30 19:35:47 +0300

    empty main error fixed

--- lib/core/initializing_app.dart
+++ lib/core/initializing_app.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app_pp/core/firebase_options.dart';
 
 import 'base_app.dart';
 
 initializingApp() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+  await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
