56e8438a0fe1d349098dd763ef4d0f423bf7176b
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-02 08:40:24 +0300

    profile page start to build

--- lib/features/pages/profile_page.dart
+++ lib/features/pages/profile_page.dart
@@ -10,14 +10,48 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
+  itemsSpaceHeight(){
+    return const SizedBox(height: 20.0,);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(children: [
-        CircleAvatar(
-          child: CircleAvatarWidget(),
-        )
-      ]),
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              itemsSpaceHeight(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(size: 30.0,Icons.arrow_back),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: InkWell(
+                      onTap:() {
+                        // todo routh setting page
+                      },
+                      child: Icon(Icons.settings),
+                    ),
+                  ),
+                ],
+              ),
+              const CircleAvatarWidget(radiusCircleAvatar: 75.0,)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
