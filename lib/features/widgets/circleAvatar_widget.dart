605be3f9cddef3d3b7a7a8ea24d070d63d4cc128
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-02 08:12:17 +0300

    circleavatar widget edited

--- lib/features/widgets/circleAvatar_widget.dart
+++ lib/features/widgets/circleAvatar_widget.dart
@@ -2,7 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class CircleAvatarWidget extends StatefulWidget {
-  const CircleAvatarWidget({Key? key}) : super(key: key);
+  final radiusCircleAvatar;
+  const CircleAvatarWidget({Key? key, this.radiusCircleAvatar}) : super(key: key);
 
   @override
   State<CircleAvatarWidget> createState() => _CircleAvatarWidgetState();
@@ -12,7 +13,17 @@ class _CircleAvatarWidgetState extends State<CircleAvatarWidget> {
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      child: Text(FirebaseAuth.instance.currentUser!.displayName.toString().split(' ').first.substring(0,1)+FirebaseAuth.instance.currentUser!.displayName.toString().split(' ').last.substring(0,1)),
+      radius: 20 ?? widget.radiusCircleAvatar,
+      child: Text(FirebaseAuth.instance.currentUser!.displayName
+              .toString()
+              .split(' ')
+              .first
+              .substring(0, 1) +
+          FirebaseAuth.instance.currentUser!.displayName
+              .toString()
+              .split(' ')
+              .last
+              .substring(0, 1)),
     );
   }
 }
