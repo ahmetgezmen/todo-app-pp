0a6cd9d633e302cf05b7cdb0cb6a568c091093ea
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-02 05:17:32 +0300

    MidWay added

---
+++ lib/core/midway.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_pp/features/pages/home_page.dart';
+
+class MidWay extends StatefulWidget {
+  const MidWay({Key? key}) : super(key: key);
+
+  @override
+  State<MidWay> createState() => _MidWayState();
+}
+
+class _MidWayState extends State<MidWay> {
+  @override
+  Widget build(BuildContext context) {
+    return const MyHomePage();
+  }
+}
