d5ebe225b38a131676b06e32d5c8e4738ed905c0
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-03 03:45:11 +0300

    user services added

---
+++ lib/features/services/users_services.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_app_pp/features/models/user_model.dart';
+
+
+CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+class UserServices{
+
+  static Future<void> addUser() {
+    final fireUser = FirebaseAuth.instance.currentUser!;
+    return users
+        .add(userModelToJson(UserModel(
+      fname: fireUser.displayName.toString(),
+      email: fireUser.email.toString(),
+      uuid: fireUser.uid.toString(),
+      phone: fireUser.phoneNumber.toString(),
+      picsUrl: fireUser.photoURL.toString(),
+      isApproved: fireUser.emailVerified
+    )))
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
+
+
+
+
+
+}
