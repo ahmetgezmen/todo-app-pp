aabf4cbcf50d2a5add01789d7edda475c7dd1dce
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-03 23:46:20 +0300

    file view model edided

--- lib/features/viewmodels/note_files_list_viewmodel.dart
+++ lib/features/viewmodels/note_files_list_viewmodel.dart
@@ -1,8 +1,20 @@
-class NoteFilesListViewModel {
-  List _modelList = [];
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app_pp/features/services/file_services.dart';
 
-  void fetching(){
-    
+class NoteFilesListViewModel extends ChangeNotifier{
+  List<QueryDocumentSnapshot<Map<String, dynamic>>>  _modelList = [];
+
+  get lenght => _modelList.length;
+
+  List<QueryDocumentSnapshot<Map<String, dynamic>>>  get modelList => _modelList;
+
+  Future<void> gettingFile() async {
+    _modelList = await FileServices.getFileLists();
+    notifyListeners();
+  }
+  fetching(){
+
   }
 
 }
