83e562bbf700781e65bb6598972c767f0a4496e6
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-03 23:45:36 +0300

    file services edided

--- lib/features/pages/home_page.dart
+++ lib/features/pages/home_page.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_app_pp/constant/base_constant.dart';
+import 'package:todo_app_pp/features/models/note_files_model.dart';
 import 'package:todo_app_pp/features/pages/profile_page.dart';
+import 'package:todo_app_pp/features/providers/file_provider.dart';
+import 'package:todo_app_pp/features/services/file_services.dart';
+import 'package:todo_app_pp/features/widgets/note_file_widget.dart';
 
 import '../widgets/circle_avatar_widget.dart';
 
@@ -18,6 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: const FloatingActionsButtonWidget(),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -26,11 +33,18 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  AppBarLeadingButton(paddingNumber: widget.paddingNumber,),
-                  Text(BaseConstant.appName, style: Theme.of(context).textTheme.titleLarge,),
-                  IconButton(onPressed: () {
-                    // todo bildirimler
-                  }, icon: const Icon(Icons.add_alert))
+                  AppBarLeadingButton(
+                    paddingNumber: widget.paddingNumber,
+                  ),
+                  Text(
+                    BaseConstant.appName,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        // todo bildirimler
+                      },
+                      icon: const Icon(Icons.add_alert))
                 ],
               ),
             ),
@@ -44,19 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
-            Expanded(
-              child: GridView.builder(
-                itemCount: 20,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 3 / 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20),
-                itemBuilder: (context, index) {
-                  return SvgPicture.asset("assets/svg/files-icon.svg");
-                },
-              ),
-            ),
+            GridViewWidget(),
           ],
         ),
       ),
@@ -66,7 +68,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class AppBarLeadingButton extends StatelessWidget {
   final double paddingNumber;
-  const AppBarLeadingButton({Key? key, required this.paddingNumber}) : super(key: key);
+  const AppBarLeadingButton({Key? key, required this.paddingNumber})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -75,10 +78,65 @@ class AppBarLeadingButton extends StatelessWidget {
       child: InkWell(
         onTap: () async {
           // Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ProfilePage(),));
-          await Scaffold.of(context).showBottomSheet((buildContext) => const ProfilePage(),).closed;
-
+          await Scaffold.of(context)
+              .showBottomSheet(
+                (buildContext) => const ProfilePage(),
+              )
+              .closed;
         },
-        child:  const CircleAvatarWidget(),
+        child: const CircleAvatarWidget(),
+      ),
+    );
+  }
+}
+
+class FloatingActionsButtonWidget extends ConsumerStatefulWidget {
+  const FloatingActionsButtonWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  ConsumerState createState() => _FloatingActionsButtonWidgetState();
+}
+
+class _FloatingActionsButtonWidgetState
+    extends ConsumerState<FloatingActionsButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () async {
+        await FileServices.addFiles('eeeee', false);
+        await ref.read(fileProvider).gettingFile();
+        setState(() {});
+      },
+      child: const Icon(Icons.add),
+    );
+  }
+}
+
+class GridViewWidget extends ConsumerWidget {
+  const GridViewWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final providerFile = ref.watch(fileProvider);
+    return Expanded(
+      child: GridView.builder(
+        itemCount: providerFile.lenght,
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        itemBuilder: (context, index) {
+          return NoteFileWidget(
+              title: noteFilesModelFromJson(
+                      jsonEncode(providerFile.modelList[index].data()))
+                  .title
+                  .toString());
+        },
       ),
     );
   }
