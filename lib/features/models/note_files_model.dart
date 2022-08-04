8759be98c707aa64ceadc5af450b5df37ec6ec0c
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-04 13:40:52 +0300

    add uid in file model

--- lib/features/models/note_files_model.dart
+++ lib/features/models/note_files_model.dart
@@ -7,11 +7,13 @@ String noteFilesModelToJson(NoteFilesModel data) => json.encode(data.toJson());
 class NoteFilesModel {
   NoteFilesModel({
     String? title,
+    String? uid,
     String? parentUid,
     List<String>? list,
     bool? isUnderTheMain,
     bool? isPrivate,}){
     _title = title;
+    _uid = uid;
     _parentUid = parentUid;
     _list = list;
     _isPrivate = isPrivate;
@@ -20,18 +22,21 @@ class NoteFilesModel {
 
   NoteFilesModel.fromJson(dynamic json) {
     _title = json['title'];
+    _uid = json['uid'];
     _parentUid = json['parentUid'];
     _list = json['list'] != null ? json['list'].cast<String>() : [];
     _isPrivate = json['isPrivate'];
     _isUnderTheMain = json['isUnderTheMain'];
   }
   String? _title;
+  String? _uid;
   String? _parentUid;
   List<String>? _list;
   bool? _isPrivate;
   bool? _isUnderTheMain;
 
   String? get title => _title;
+  String? get uid => _uid;
   String? get parentUid => _parentUid;
   List<String>? get list => _list;
   bool? get isPrivate => _isPrivate;
@@ -40,6 +45,7 @@ class NoteFilesModel {
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['title'] = _title;
+    map['uid'] = _uid;
     map['parentUid'] = _parentUid;
     map['list'] = _list;
     map['isPrivate'] = _isPrivate;
