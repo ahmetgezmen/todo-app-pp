f1cb184568466942fccf1691a3d3b82ecef38378
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-08-07 22:17:20 +0300

    Text File Config added

---
+++ lib/config/text_file_config.dart
@@ -0,0 +1,13 @@
+class TextFileConfig{
+
+  static const lineCatagory = {
+    "header_1" : "header_1",
+    "header_2" : "header_2",
+    "header_3" : "header_3",
+    "code":"code",
+    "text": "text",
+  };
+
+
+
+}
